5231298b58df75073cf365f707f9d71e001a0474
s-aibar <sara.aibar@kuleuven.vib.be>
2021-03-16 16:19:10 +0100

    rankings: indexCol can now be anywhere

--- R/00_importRankings.R
+++ R/00_importRankings.R
@@ -1,12 +1,13 @@
 #' @title Import the motif databases for RcisTarget.
 #' @param dbFile .feather or .parquet file containing the rankings
+#' @param indexCol Column name containing the feature IDs (e.g. motif names or chip-seq tracks). If NULL, it will take the first column.
 #' @param columns Columns to load from the .feather or .parquet file
 #' (e.g. to read only specific genes or regions)
-#' @param dbDescr Description fields (not used internally) e.g.:
+#' @param warnMissingColumns If 'columns' is provided, warn if any ID is not available in the rankings? 
+#' @param dbDescr Description fields (not used in the analysis, just for convenience of the user) 
+#' e.g.:
 #' \code{dbDescr=list(colType="gene", rowType="motif",
 #' org="Human", genome="hg19", maxRank=Inf, description="")}
-#' @param indexCol Column name containing the feature IDs (e.g. motif names or chip-seq tracks). 
-#' @param warnMissingColumns If 'columns' is provided, warn if any ID is not available in the rankings? 
 #' @description
 #' The rankings are typically loaded from a .feather or .parquet file
 #' with \code{importRankings()}.
@@ -44,62 +45,66 @@
 #' @import arrow
 #' @importFrom utils read.table
 #' @export
-importRankings <- function(dbFile, columns=NULL, dbDescr=NULL, indexCol="features", warnMissingColumns=TRUE)
+importRankings <- function(dbFile, indexCol=NULL, columns=NULL, warnMissingColumns=TRUE, dbDescr=NULL)
 {
   dbFile <- path.expand(dbFile)
   if(!file.exists(dbFile)) stop("File does not exist: ", dbFile)
-
+  
+  allColumns <- getColumnNames(dbFile)
+  if(is.null(indexCol)) {
+    indexCol <- allColumns[1]
+  }else{
+    if(!indexCol %in% allColumns) stop(paste0("The index column '", indexCol,"' is not available in the file."))
+  }
+  
+  ## If columns are subset: add the index column
   if(!is.null(columns)){
-    missingColumns <- columns[which(!columns %in% getColumnNames(dbFile))]
+    missingColumns <- columns[which(!columns %in% allColumns)]
     if(length(missingColumns)>0 & warnMissingColumns)
     {
       warning("The following columns are missing from the database: ", paste(missingColumns, collapse=", "))
-      columns <- columns[which(columns %in% getColumnNames(dbFile))]
+      columns <- columns[which(columns %in% allColumns)]
     }
     columns <- unique(c(indexCol, columns))
   }
+  
+  ## Read file (format chosen based on extension)
   extension <- strsplit(dbFile, "\\.") [[1]][length(strsplit(dbFile, "\\.") [[1]])]
   if (extension == 'feather'){
     rnks <- arrow::read_feather(dbFile, col_select=!!columns, mmap = TRUE); dim(rnks)
-    nColsInDB <- length(names(arrow::FeatherReader$create(arrow::ReadableFile$create(dbFile))))-1
+    nColsInDB <- length(allColumns)-1  #names(arrow::FeatherReader$create(arrow::ReadableFile$create(dbFile)))
   }else if (extension == "parquet"){
     rnks <- read_parquet(dbFile, columns = !!columns)
-    pq <- arrow::ParquetFileReader(dbFile)
-    nColsInDB <- pq$GetSchema()$num_fields()-1
+    nColsInDB <- length(allColumns)-1 # pq <- arrow::ParquetFileReader(dbFile); pq$GetSchema()$num_fields()-1
   }else{
     stop("Database format must be feather or parquet.")
   }
-
-  dbFile_descr <- gsub(paste0(".", extension),".descr", dbFile, fixed=TRUE)
+  
+  # The index column should be the first one
+  if(!is.null(indexCol)) 
+  {
+    rnks <- rnks %>% relocate(!!indexCol)
+  }
+  
+  # Add description if available
   if(!is.null(dbDescr))
   {
     dbDescr <- as.matrix(dbDescr)
-    if(file.exists(dbFile_descr))
-      warning("Ignoring the DB file description (.descr)")
   } else {
-    if(file.exists(dbFile_descr))
-    {
-      dbDescr <- utils::read.table(file=dbFile_descr,
-                            sep = "\t", row.names=1, stringsAsFactors=FALSE)
-      message("Imported description file:\n",
-              paste("\t", unname(sapply(rownames(dbDescr),
-            function(x) paste(x, dbDescr[x,1], sep=": "))), collapse="\n"))
-    }else{
-      # If not provided: keep empty
-      dbDescr <- as.matrix(list(colType="column",
-                                rowType="row",
-                                org="",
-                                genome="",
-                                nColsAvailable=nColsInDB,
-                                maxRank = Inf,
-                                description=""))
-    }
+    # If not provided: keep empty
+    dbDescr <- as.matrix(list(colType="column",
+                              rowType=indexCol,
+                              org="",
+                              genome="",
+                              nColsAvailable=nColsInDB,
+                              maxRank = Inf,
+                              description=""))
   }
-
+  
+  # Create object
   dbDescr["nColsAvailable",] <- nColsInDB
   dbDescr["description",] <- paste0(dbDescr["description",],
                                     " [Source file: ", basename(dbFile),"]")
-
   rownames(dbDescr) <- tolower(rownames(dbDescr))
   new("rankingRcisTarget",
       rankings=rnks,
@@ -122,7 +127,7 @@ getRowNames <- function(dbFile)
     ret <- unname(unlist(arrow::read_feather(path.expand(dbPath), col_select=1, mmap = TRUE)))
   }
   else if (extension == "parquet"){
-     stop("Not implemented") # TODO: add arrow
+    stop("Not implemented") # TODO: add arrow
   }
   return(ret)
 }
